926faa01af56ad64a5430df58d5f8614a34ff132
Phan Anh Tran <kishimotovn@gmail.com>
2024-07-11 15:25:50 +0100

    Initial Commit

---
+++ XMTechnicalTest/XMTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMTechnicalTestApp.swift
+//  XMTechnicalTest
+//
+//  Created by Phan Anh Tran on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct XMTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
